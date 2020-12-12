5aec14bba86a85351d7e5769de18555eba875b54
Lakr Aream <master@233owo.com>
2020-12-12 20:15:08 +0800

    Exclusive Update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/DimaRU/Libssh2Prebuild/releases/download/1.9.0+OpenSSL_1_1_1h/CSSH-1.9.0+OpenSSL_1_1_1h.xcframework.zip",
-                      checksum: "6e169e26136a73038b627c899b0ea07efae9aa25359b559c8fae0c1689ad99cf")
+                      url: "https://github.com/Co2333/Libssh2Prebuild/releases/download/ExclusiveBuild-20201212/CSSH-1.9.0+OpenSSL_1_1_1h.xcframework.zip",
+                      checksum: "e10b3a3553bf5f21dc50025b522e84fc4d05d8efca62a19a0c2929d382c1bac9")
     ]
 )
